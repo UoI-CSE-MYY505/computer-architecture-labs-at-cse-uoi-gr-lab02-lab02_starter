a43eedd2061d720035be801ef7fd5b66f9f6cf7e
Johnpap3 <cs05532@uoi.gr>
2024-10-22 16:18:32 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -9,10 +9,22 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a1, 7    # unsigned
     li a2, 1
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+li s0,0
+beq a1, x0,done
+
+mv t0,a0
+mv t1,a1
+loop_start:
+lw t2,0(t0)
+beq t2,a2,max
+no_match:
+addi t0,t0,4
+addi t1,t1,-1
+bnez t1, loop_start
+j done
+match:
+mv s0,t0
+j no_match
 done:
     addi a7, zero, 10 
     ecall
